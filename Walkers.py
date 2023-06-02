140a9e63886babc6e41f58071dd5cb2b90a2cec9
NithinBalachandar <70101919+NithinBalachandar@users.noreply.github.com>
2023-06-02 08:06:57 -0600

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,21 @@
+
+import cv2
+
+body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml') 
+vid = cv2.VideoCapture("walking.avi")
+
+while(True):
+    ret, frame = vid.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bodies = body_cascade.detectMultiScale(gray, 1.1, 5)
+
+    for (x, y, w, h) in bodies:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(25) == 32:
+        break
+
+vid.release()
+cv2.destroyAllWindows()
